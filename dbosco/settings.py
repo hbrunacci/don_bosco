2af2aa48a05e67eb0ee61eb68a7323945d9d55b2
Hernan <hbrunacci@gmail.com>
2018-09-03 04:11:24 -0300

    change DB settings

--- dbosco/settings.py
+++ dbosco/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'dbosco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'dbosco',
+#        'USER': 'dbosco',
+#        'PASSWORD': 'd0nb0sc02018',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432,
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbosco',
-        'USER': 'dbosco',
-        'PASSWORD': 'd0nb0sc02018',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
